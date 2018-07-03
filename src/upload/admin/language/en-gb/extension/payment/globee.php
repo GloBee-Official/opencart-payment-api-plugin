3ae57a80120f1e4b652f6c456683f9cb1ed34bfb
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-03 13:45:33 +0200

    Created a language file for english text

---
+++ src/upload/admin/language/en-gb/extension/payment/globee.php
@@ -0,0 +1,21 @@
+<?php
+
+$_['heading_title'] = 'GloBee';
+
+$_['notification_success'] = 'You have successfully updated the GloBee plugin';
+$_['notification_log_success'] = 'You have successfully cleared the GloBee log';
+
+$_['button_save'] = 'Save';
+$_['button_cancel'] = 'Cancel';
+$_['button_clear'] = 'Clear';
+
+$_['tab_settings'] = 'Settings';
+$_['tab_log'] = 'Log';
+
+$_['label_edit'] = 'Edit GloBee';
+$_['label_enabled'] = 'Enabled';
+$_['label_payment_api_key'] = 'Payment API Key';
+
+$_['text_payment'] = 'Payment';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
