f982a345734ccb5fe413220e17a39059cf027b40
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-03 13:37:39 +0200

    Created the robo file with the build script

---
+++ RoboFile.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class RoboFile
+ */
+class RoboFile extends \Robo\Tasks
+{
+    /**
+     * Build the our distribution package
+     */
+    public function build()
+    {
+        // Delete existing distribution directory
+        $this->taskDeleteDir('dist')
+            ->run();
+
+        // Copy the src directory into the stack
+        $this->taskFileSystemStack()
+            ->mirror('src', 'dist/globee-opencart-3.0.2')
+            ->run();
+
+        // Copy the SDK into the stack
+        $this->taskFileSystemStack()
+            ->mirror('vendor/globee/payment-api/src/', 'dist/globee-opencart-3.0.2/upload/system/library/globee')
+            ->run();
+
+        // Build the zip
+        $this->taskExec('zip')
+            ->dir('dist/globee-opencart-3.0.2')
+            ->arg('-r')
+            ->arg('../globee-opencart-3.0.2.ocmod.zip')
+            ->arg('./')
+            ->run();
+    }
+}
