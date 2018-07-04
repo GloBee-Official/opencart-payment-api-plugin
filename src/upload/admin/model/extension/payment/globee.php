1ed0bbcd02e48a2f3cbc974f0367c72e22d4c721
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-04 11:04:10 +0200

    Created a model file to handle the creation and deletion of keys during the installation and deletion process

---
+++ src/upload/admin/model/extension/payment/globee.php
@@ -0,0 +1,31 @@
+<?php
+
+class ModelExtensionPaymentGlobee extends Model
+{
+    public function install()
+    {
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_enabled','0','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_livenet','1','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_payment_api_key','','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_risk_speed','medium','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_paid_status',2,'0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_confirmed_status',2,'0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_completed_status',5,'0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_notification_url','".$this->url->link('extension/payment/globee/callback', $this->config->get('config_secure'))."','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_redirect_url','".$this->url->link('extension/payment/globee/success', $this->config->get('config_secure'))."','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_logging','1','0');");
+    }
+
+    public function uninstall()
+    {
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_enabled';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_livenet';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_payment_api_key';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_risk_speed';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_confirmed_status';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_completed_status';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_notification_url';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_redirect_url';");
+        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_logging';");
+    }
+}
