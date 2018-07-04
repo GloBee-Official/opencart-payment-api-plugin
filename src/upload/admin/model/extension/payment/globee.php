e379ab18dabc36af316000c0ee6bf11afc1ff27e
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-04 15:32:26 +0200

    Modified the installation code to strip out the admin part of the success and redirect urls, and used the settings module to delete database entries during uninstallation

--- src/upload/admin/model/extension/payment/globee.php
+++ src/upload/admin/model/extension/payment/globee.php
@@ -10,24 +10,17 @@ class ModelExtensionPaymentGlobee extends Model
         $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_payment_api_key','','0');");
         $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_risk_speed','medium','0');");
         $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_paid_status',2,'0');");
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_confirmed_status',2,'0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_confirmed_status',15,'0');");
         $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_completed_status',5,'0');");
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_notification_url','".$this->url->link('extension/payment/globee/callback', $this->config->get('config_secure'))."','0');");
-        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_redirect_url','".$this->url->link('extension/payment/globee/success', $this->config->get('config_secure'))."','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_notification_url','".str_replace("admin/", "", $this->url->link('extension/payment/globee/callback', $this->config->get('config_secure')))."','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_redirect_url','".str_replace("admin/", "", $this->url->link('extension/payment/globee/success', $this->config->get('config_secure')))."','0');");
         $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_logging','1','0');");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "setting` (`store_id`,`code`,`key`,`value`,`serialized`) VALUES ('0','payment_globee','payment_globee_geo_zone_id','0','0');");
     }
 
     public function uninstall()
     {
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_status';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_sort_order';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_livenet';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_payment_api_key';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_risk_speed';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_confirmed_status';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_completed_status';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_notification_url';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_redirect_url';");
-        $this->db->query("DELETE FROM `" . DB_PREFIX . "setting` WHERE key = 'payment_globee_logging';");
+        $this->load->model('setting/setting');
+        $this->model_setting_setting->deleteSetting('payment_globee');
     }
 }
