944a96da6115f2e19b7b4b5e3c014b8b3a56ae0e
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-04 15:32:43 +0200

    Created catalog language file (Frontend)

---
+++ src/upload/catalog/language/en-gb/extension/payment/globee.php
@@ -0,0 +1,6 @@
+<?php
+
+$_['heading_title'] = 'GloBee';
+$_['text_title'] = 'Pay with GloBee';
+$_['warning_testnet'] = 'Warning: You are connected to the Globee Test System. Use Testnet funds to test the process.';
+$_['button_confirm'] = 'Confirm';
