3b7e1fc12191177984018d0c3bb658dd3bd71491
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-04 11:03:43 +0200

    Added more strings to the language file

--- src/upload/admin/language/en-gb/extension/payment/globee.php
+++ src/upload/admin/language/en-gb/extension/payment/globee.php
@@ -4,6 +4,12 @@ $_['heading_title'] = 'GloBee';
 
 $_['notification_success'] = 'You have successfully updated the GloBee plugin';
 $_['notification_log_success'] = 'You have successfully cleared the GloBee log';
+$_['notification_error_notification_url'] = 'The notification URL is required and must be a valid URL';
+$_['notification_error_redirect_url'] = 'The redirect URL is required and must be a valid URL';
+$_['notification_error_payment_api_key_enabled'] = 'The plugin cannot be activated without a Payment API Key';
+$_['notification_error_notification_url_enabled'] = 'The plugin cannot be activated without a Callback URL';
+$_['notification_error_redirect_url_key_enabled'] = 'The plugin cannot be activated without a Redirect URL';
+$_['notification_error_payment_api_key'] = 'Please enter the Payment API Key';
 
 $_['button_save'] = 'Save';
 $_['button_cancel'] = 'Cancel';
@@ -14,8 +20,21 @@ $_['tab_log'] = 'Log';
 
 $_['label_edit'] = 'Edit GloBee';
 $_['label_enabled'] = 'Enabled';
+$_['label_network'] = 'Network';
 $_['label_payment_api_key'] = 'Payment API Key';
+$_['label_risk_speed'] = 'Notification Speed';
+$_['label_paid_status'] = 'Paid Status';
+$_['label_confirmed_status'] = 'Confirmed Status';
+$_['label_completed_status'] = 'Completed Status';
+$_['label_notification_url'] = 'Callback URL (IPN)';
+$_['label_redirect_url'] = 'Redirect URL';
+$_['label_debugging'] = 'Debugging';
 
 $_['text_payment'] = 'Payment';
 $_['text_enabled'] = 'Enabled';
 $_['text_disabled'] = 'Disabled';
+$_['text_low'] = 'Low (Secure)';
+$_['text_medium'] = 'Medium';
+$_['text_high'] = 'High (Fast)';
+$_['text_livenet'] = 'Livenet';
+$_['text_testnet'] = 'Testnet';
