319cdd5273f05104dc7ed27b50f7b42258cc1b3d
Gustav Trenwith <gtrenwith@gmail.com>
2018-07-06 11:24:32 +0200

    Added language keys to display our logo

--- src/upload/admin/language/en-gb/extension/payment/globee.php
+++ src/upload/admin/language/en-gb/extension/payment/globee.php
@@ -1,6 +1,8 @@
 <?php
 
 $_['heading_title'] = 'GloBee';
+$_['text_globee'] = '<a href="http://globee.com" target="_blank"><img src="view/image/payment/globee.png"></a>';
+$_['globee'] = 'GloBee';
 
 $_['notification_success'] = 'You have successfully updated the GloBee plugin';
 $_['notification_log_success'] = 'You have successfully cleared the GloBee log';
@@ -9,7 +11,7 @@ $_['notification_error_redirect_url'] = 'The redirect URL is required and must b
 $_['notification_error_payment_api_key_enabled'] = 'The plugin cannot be activated without a Payment API Key';
 $_['notification_error_notification_url_enabled'] = 'The plugin cannot be activated without a Callback URL';
 $_['notification_error_redirect_url_key_enabled'] = 'The plugin cannot be activated without a Redirect URL';
-$_['notification_error_payment_api_key'] = 'Please enter the Payment API Key';
+$_['notification_error_payment_api_key'] = 'Please enter a Payment API Key';
 $_['notification_error_communication_failed'] = 'The Payment API Key cannot communicate with GloBee. Check that the key is correct for the set network.';
 
 $_['button_save'] = 'Save';
